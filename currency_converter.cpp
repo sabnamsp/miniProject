e448e59c8a18cb6ef33a62d300748080215b3600
Sabnam Parvin Bristy <114932621+sabnamsp@users.noreply.github.com>
2023-08-02 00:00:50 +0600

    Add files via upload

---
+++ currency_converter.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    float dollar;
+    float pound = 0.78;
+    float franc = 0.88;
+    float euro = 0.91;
+    float yen = 143.53;
+    float bdt = 108.57;
+    float rupee = 74.52;
+    float peso = 20.12;
+    float lira = 8.45;
+    float ruble = 73.25;
+    float yuan = 6.45;
+    float aud = 1.35;
+    float cad = 1.25;
+    float nzd = 1.42;
+    float chf = 0.93;
+    float sgd = 1.33;
+    float sek = 8.62;
+    float nok = 8.82;
+    float zar = 14.32;
+    float brl = 5.24;
+    float aed = 3.67;
+    float krw = 1174.15;
+
+    cout << "Enter the Dollars for Conversion: ";
+    cin >> dollar;
+
+    cout << "British Pounds: " << pound * dollar << endl;
+    cout << "French Francs: " << franc * dollar << endl;
+    cout << "Euros: " << euro * dollar << endl;
+    cout << "Japanese Yen: " << yen * dollar << endl;
+    cout << "Bangladeshi Taka: " << bdt * dollar << endl;
+    cout << "Indian Rupees: " << rupee * dollar << endl;
+    cout << "Mexican Pesos: " << peso * dollar << endl;
+    cout << "Turkish Lira: " << lira * dollar << endl;
+    cout << "Russian Rubles: " << ruble * dollar << endl;
+    cout << "Chinese Yuan: " << yuan * dollar << endl;
+    cout << "Australian Dollars: " << aud * dollar << endl;
+    cout << "Canadian Dollars: " << cad * dollar << endl;
+    cout << "New Zealand Dollars: " << nzd * dollar << endl;
+    cout << "Swiss Francs: " << chf * dollar << endl;
+    cout << "Singapore Dollars: " << sgd * dollar << endl;
+    cout << "Swedish Kronor: " << sek * dollar << endl;
+    cout << "Norwegian Kroner: " << nok * dollar << endl;
+    cout << "South African Rand: " << zar * dollar << endl;
+    cout << "Brazilian Reais: " << brl * dollar << endl;
+    cout << "UAE Dirhams: " << aed * dollar << endl;
+    cout << "South Korean Won: " << krw * dollar << endl;
+
+    return 0;
+}
